225e6ac34b5f8e9465a33ca1ac836a0ea807e710
王亚威 <wangyw@yunrong.cn>
2022-03-25 17:19:08 +0800

    feat: 添加中等网接口

--- project/kylin-guarantee/financeRegister.js
+++ project/kylin-guarantee/financeRegister.js
@@ -1,52 +1,178 @@
+/**
+ * @Author: wangyw26123
+ * @Description: 中登网
+ * @Date: Created in 2022-02-16 10:15:51
+ * @Modifed By:
+ */
 
 const routes = [
   {
-    name: '',
-    path: '/v1/checkInfo/queryPage',
+    name: '动产查询列表',
+    path: '/guaranteeRegister/directConnectionRegisterQueryPage',
     context: (ctx) => {
+      const mockData = $Mock({
+        'list|30-100': [{
+          'registerThirdId': 'LN@natural(10000000, 99999999)',
+          'mortgagor': '@cname',
+          'managerName': '@cname',
+          'secondary_investigator_name': '@cname',
+          'registerStartDate': '@datetime',
+          'registerEndDate': '@datetime',
+          'registerType|1': [
+            '001',
+            '002',
+            '003',
+          ],
+          'registerBusinessType|1': [
+            '001',
+            '002',
+            '003',
+          ],
+          'guarantorInfoList|1-10': [
+            {
+              affiliatedName: '@cname'
+            }
+          ],
+          'securedPartyInfoList|1-10': [
+            {
+              affiliatedName: '@cname'
+            }
+          ],
+        }],
+      });
+      mockData.total = mockData.list.length;
       ctx.statusCode = 200;
       ctx.body = {
-        data: $Mock({
-          'list|30-100': [{
-            'check_id': 'LN@natural(10000000, 99999999)',
-            'user_name': '@cname',
-            'managerName': '@cname',
-            'secondary_investigator_name': '@cname',
-            'create_time': '@datetime',
-            'investigateTime': '@datetime',
-            'createTime': '@datetime',
-            'handle_time': '@datetime',
-            'check_type|1': [
-              '001',
-              '002',
-              '003'
-            ],
-            'certificate_kind': '居民身份证',
-            'certificate_no': /(^\d{15}$)|(^\d{17}(\d|X|x)$)/,
-            'user_mobile': /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/,
-            'check_biz_type|1': [
-              '001',
-              '002'
-            ],
-            'contract_id': 'LN@natural(1000000, 9999999)',
-            'credit_limit_id': 'LN@natural(1000000, 9999999)',
-            'secondary_investigator_id': 'LN@natural(1000000, 9999999)',
-            'buiness': 1,
-            'product_name': '@csentence(2, 5)',
-            'product_id': 'LN@natural(1000000, 9999999)',
-            'status|1': [
-              '001',
-              '002',
-              '003'
-            ]
-          }],
-          'total|30-100': 30,
-        }),
+        data: mockData,
         httpStatus: 200,
-        responseType: "HSJRY_SUCCESS",
-      }
-    }
-  }
-]
+        responseType: 'HSJRY_SUCCESS',
+      };
+    },
+  },
+  {
+    name: '查询动产登记列表',
+    path: '/v1/guaranteeRegister/queryRegisterPage',
+    context: (ctx) => {
+      const mockData = $Mock({
+        'list|30-100': [{
+          'registerId': 'LN@natural(10000000, 99999999)',
+          'registerBusinessType|1': [
+            '002',
+          ],
+          'guarantorInfoList|1-10': [
+            {
+              affiliatedName: '@cname'
+            }
+          ],
+          'securedPartyInfoList|1-10': [
+            {
+              affiliatedName: '@cname'
+            }
+          ],
+          relationSystem: '001',
+          'relationBizType|1': [
+            '001'
+          ],
+          'relationId': 'LN@natural(10000000, 99999999)',
+          'registerStartDate': '@datetime',
+          'registerEndDate': '@datetime',
+          'registerStatus|1': [
+            '001',
+            '002',
+            '003',
+            '004',
+          ],
+          'operateName': '@cname',
+          'operateOrganName': '云科',
+          'createTime': '@datetime',
+          'updateTime': '@datetime',
+        }],
+      });
+      mockData.total = mockData.list.length;
+      ctx.statusCode = 200;
+      ctx.body = {
+        data: mockData,
+        httpStatus: 200,
+        responseType: 'HSJRY_SUCCESS',
+      };
+    },
+  },
+  {
+    name: '查询担保登记详情',
+    path: '/v1/guaranteeRegister/queryDetail',
+    context: (ctx) => {
+      const mockData = $Mock({
+        basicInfo: {
+          registerType: '',
+          'registerTerm|10-99': 34,
+          relationId: '@id'
+        },
+        assetInfo: {
+          contractNo: 'LN@natural(10000000, 99999999)',
+          contractCurrency: '',
+          contractAmount: '@natural(10000000, 99999999)',
+          propertyCurrency: '',
+          propertyAmount: '@natural(10000000, 99999999)',
+          propertyDesc: '3434',
+        },
+        'transferorList|6': [
+          {
+            type: '',
+            name: '@name',
+            certificateKind: '居民身份证',
+            certificateNo: /(^\d{15}$)|(^\d{17}(\d|X|x)$)/
+          }
+        ],
+        'assigneeList|6': [
+          {
+            type: '',
+            name: '@name',
+            certificateKind: '居民身份证',
+            certificateNo: /(^\d{15}$)|(^\d{17}(\d|X|x)$)/
+          }
+        ],
+        'attachmentList|6': [
+          {
+            id: '@natural(10000000, 99999999)',
+            name: '@cname',
+            size: '',
+            uploadDate: '@datetime',
+            uploader: '@name'
+          }
+        ],
+      });
+      ctx.statusCode = 200;
+      ctx.body = {
+        data: mockData,
+        httpStatus: 200,
+        responseType: 'HSJRY_SUCCESS',
+      };
+    },
+  },
+  {
+    name: '查询担保担保登记记录',
+    path: '/v1/guaranteeRegister/queryRecordPage',
+    context: (ctx) => {
+      const mockData = $Mock({
+        'list|30-100': [{
+          'modifyCode|100-999': 222,
+          registerType: '',
+          registerAuthorizer: '@cname',
+          filingNo: '@cname',
+          registerResult: '',
+          registerDate: '@datetime',
+          remark: '@cparagraph()',
+        }],
+      });
+      mockData.total = mockData.list.length
+      ctx.statusCode = 200;
+      ctx.body = {
+        data: mockData,
+        httpStatus: 200,
+        responseType: 'HSJRY_SUCCESS',
+      };
+    },
+  },
+];
 
 module.exports = routes;
