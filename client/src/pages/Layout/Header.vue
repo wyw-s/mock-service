778472248bc839476b8031da048d13b3c44215f5
王亚威26123 <wangyw26123@yunrong.cn>
2022-12-03 21:14:14 +0800

    feat: 添加侧边栏菜单

---
+++ client/src/pages/Layout/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="header">
+        网站头部
+    </div>
+</template>
+
+<style scoped lang="less">
+.header {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    //background: #ccc;
+}
+</style>
--- client/src/pages/Layout/index.vue
+++ client/src/pages/Layout/index.vue
@@ -1,14 +1,24 @@
+<script setup>
+import SiderBar from './SiderBar.vue';
+import Header from './Header.vue';
+
+</script>
+
 <template>
     <div class="common-layout">
         <el-container>
-            <el-aside width="200px">Aside</el-aside>
+            <el-aside width="200px">
+                <SiderBar />
+            </el-aside>
             <el-container>
-                <el-header>Header</el-header>
+                <el-header>
+                    <Header />
+                </el-header>
                 <el-container class="layout-warpper">
                     <el-main>
-                        <router-view></router-view>
+                        <router-view />
                     </el-main>
-                    <el-footer>Footer</el-footer>
+                    <!--<el-footer>Footer</el-footer>-->
                 </el-container>
             </el-container>
         </el-container>
@@ -17,10 +27,16 @@
 
 <style scoped lang="less">
 .common-layout {
+    height: 100%;
     background: #ffffff;
 
+    .el-header {
+        height: 45px;
+        box-shadow: 0px 4px 8px 0px #e1e4e3;
+    }
+
     .layout-warpper {
-        height: calc(100vh - 60px);
+        height: calc(100vh - 46px);
         overflow: auto;
     }
 }
