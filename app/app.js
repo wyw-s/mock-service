ec69ea2ef2639a3763f1508a216b76b757b5cc6a
王亚威26123 <wangyw26123@yunrong.cn>
2022-12-11 23:05:57 +0800

    feat: 托管静态资源

--- app/app.js
+++ app/app.js
@@ -4,20 +4,19 @@ const logger = require('koa-logger');
 const chalk = require('chalk');
 const moment = require('moment');
 const Mock = require('mockjs');
-const middleware = require('./middlewares')
+const serve = require('koa-static');
+const path = require('path');
+const middleware = require('./middlewares');
 const routers = require('./router');
 const app = new Koa();
 global.$Mock = Mock.mock;
 
+app.use(serve(path.join(__dirname, '../client/dist')));
 app.use(koaBody());
-app.use(middleware.util)
+app.use(middleware.util);
 app.use(logger((str) => {
-    console.log(`[${ chalk.gray(moment().format('YYYY-MM-DD HH:mm:ss')) }]`, str);
+  console.log(`[${ chalk.gray(moment().format('YYYY-MM-DD HH:mm:ss')) }]`, str);
 }));
-
-// Object.keys(routers).forEach(key => {
-//     app.use(routers[key].routes()).use(routers[key].allowedMethods());
-// });
 app.use(routers.mock.routes()).use(routers.mock.allowedMethods());
 app.use(routers.api.routes()).use(routers.api.allowedMethods());
 
