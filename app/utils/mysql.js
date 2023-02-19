95c60c66692ad152ce9c5d93d7353a2ee2227ead
Yahweh <wyw.s@foxmail.com>
2023-02-19 18:08:04 +0800

    feat: 更新 mysql 密码

--- app/utils/mysql.js
+++ app/utils/mysql.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   port: '3306',
   user: 'root',
-  password: 'root',
+  password: 'Wangyawei@520',
   database: 'mock_service',
   waitForConnections: true,
   connectionLimit: 10,
